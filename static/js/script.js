27083bee128645d4447858c7d265d35b73fa7ef2
ann <ann@sanuk.ltd>
2025-03-21 03:09:29 +0630

    update recommendation ui

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.getElementById('particles-js')) {
         particlesJS('particles-js', {
             particles: {
-                number: { value: 250,density: { enable: true, value_area: 800 } },
+                number: { value: 200,density: { enable: true, value_area: 800 } },
                 color: { value: '#daa7e8' },
                 shape: { type: 'image',
                     image:{
